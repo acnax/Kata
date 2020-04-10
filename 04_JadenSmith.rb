e1f114bce93b718df9490f7edd56cf4c018e857f
acnax <alexandrelacroix.dev@gmail.com>
2020-04-10 23:56:15 +0200

    Jaden

--- 04_JadenSmith.rb
+++ 04_JadenSmith.rb
@@ -1,10 +1,19 @@
 class String
   def toJadenCase
-  	
- 
-    
+  	self.split.map(&:capitalize).join(" ")
+=begin  	tab_maj = []
+  	tab_word = scan(/\S+/)
+  	tab_word.each do |word|
+  		tab_maj << word.capitalize
+  	end
+
+  	return tab_maj.join(" ") 
+=end
   end
+
+	
 end
 
-
-"test test test test".toJadenCase()
+notJaden = "How can mirrors be real if our eyes aren't real"
+puts "Not Jaden : #{notJaden}"
+puts "Jaden :  #{notJaden.toJadenCase()}"
